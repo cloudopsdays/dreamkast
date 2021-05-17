bf61d0be2994666e76f28bbb63d166f3651465b5
Kazuto Kusama <jacopen@gmail.com>
2021-05-18 00:35:12 +0900

    Improve UI

--- spec/requests/talks_spec.rb
+++ spec/requests/talks_spec.rb
@@ -164,7 +164,7 @@ describe TalksController, type: :request do
             get '/cndt2020/talks/2'
             expect(response).to be_successful
             expect(response).to have_http_status '200'
-            expect(response.body).to include 'タイムテーブル'
+            expect(response.body).to include 'セッション一覧'
             expect(response.body).to include talk2.title
           end
 
