b4bd8f0bb1d53bc8bfe74c81625c0366ee7949c5
Kazuto Kusama <jacopen@gmail.com>
2021-07-13 20:26:23 +0900

    Modify booth list to sort by sponsor rank

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -60,6 +60,19 @@ class ApplicationController < ActionController::Base
 
     render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
   end
+  
+  def sponsor_logo_class(sponsor_type)
+    case sponsor_type.name
+    when "Diamond", "Special Collaboration"
+      "col-12 col-md-4 my-3 m-md-3"
+    when "Platinum"
+      "col-12 col-md-3 my-3 m-md-3"
+    when "Gold", "Booth", "Mini Session", "CM", "Tool", "Logo"
+      "col-12 col-md-2 my-3 m-md-3"
+    else
+      "col-12 col-md-3 my-3 m-md-3"
+    end
+  end
 
   helper_method :sponsor_logo_class, :days
   private
